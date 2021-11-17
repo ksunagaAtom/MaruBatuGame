8f3cebb0c44e26c601ba58cf82565b90e3028909
ksunagaAtom <k.sunaga@atomitech.jp>
2021-11-17 16:26:16 +0900

    option_replay

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import tkinter as tk
+import time
 
 #Frameを拡張したクラス
 class MyFrame(tk.Frame):
@@ -7,7 +8,8 @@ class MyFrame(tk.Frame):
         self.turnFlag = 1
         self.turnCount = 0
         self.board = [[0,0,0],[0,0,0],[0,0,0]]
-        self.boardFlag = [[True]*3]*3
+        self.boardRe = []
+        self.boardFlag = [[True]*3,[True]*3,[True]*3]
 
         self.b1 = tk.Button(self, text='　', command=lambda:self.key(1,0,0))
         self.b2 = tk.Button(self, text='　', command=lambda:self.key(2,0,1))
@@ -18,7 +20,8 @@ class MyFrame(tk.Frame):
         self.b7 = tk.Button(self, text='　', command=lambda:self.key(7,2,0))
         self.b8 = tk.Button(self, text='　', command=lambda:self.key(8,2,1))
         self.b9 = tk.Button(self, text='　', command=lambda:self.key(9,2,2))
-        self.b_reset = tk.Button(self, text='C', command=lambda:self.c_key())
+        self.b_clear = tk.Button(self, text='C', command=lambda:self.c_key())
+        self.b_replay = tk.Button(self, text='R', command=lambda:self.r_key())
         self.message = tk.Label(self, text='メッセージ')
 
 
@@ -32,7 +35,8 @@ class MyFrame(tk.Frame):
         self.b8.grid(row=3, column=1)
         self.b9.grid(row=3, column=2)
         self.message.grid(row=4, column=0, columnspan=4)
-        self.b_reset.grid(row=5, column=0)
+        self.b_clear.grid(row=5, column=0)
+        self.b_replay.grid(row=5, column=1)
 
     #水平方向での勝ちの判定
     def check_board_horizontal(self,turnFlag):
@@ -40,12 +44,14 @@ class MyFrame(tk.Frame):
             if (self.board[i][0] == self.turnFlag) and (self.board[i][1] == self.turnFlag) and (self.board[i][2] == self.turnFlag):
                 return True
         return False
+
     #垂直方向での勝ちの判定
     def check_board_vertical(self,turnFlag):
         for j in range (3):
             if (self.board[0][j] == self.turnFlag) and (self.board[1][j] == self.turnFlag) and (self.board[2][j] == self.turnFlag):
                 return True
         return False
+
     #対角方向での勝ちの判定
     def check_board_diagonal(self,turnFlag):
         if (self.board[0][0] == self.turnFlag) and (self.board[1][1] == self.turnFlag) and (self.board[2][2] == self.turnFlag):
@@ -56,6 +62,7 @@ class MyFrame(tk.Frame):
         if (self.board[0][2] == self.turnFlag) and (self.board[1][1] == self.turnFlag) and (self.board[2][0] == self.turnFlag):
             return True
         return False
+
     #勝利判定メソッド    
     def is_win_simple(self,turnFlag):
         if self.check_board_horizontal(self.turnFlag):
@@ -68,6 +75,7 @@ class MyFrame(tk.Frame):
             return True
         return False
 
+    #オプション以外のボタンメソッド
     def key(self,n,row,column):
         #keyメソッドの制限
         if self.turnCount < 9 and self.boardFlag[row][column] == True:
@@ -98,12 +106,9 @@ class MyFrame(tk.Frame):
 
             #リストに代入
             self.board[row][column] = self.turnFlag
-            print(self.board)
-            
+
             #勝敗判定
             WoL = self.is_win_simple(self.turnFlag)
-            print(WoL)
-            print('turnCount',self.turnCount)
 
             #メッセージに勝者表示
             if WoL == True:
@@ -112,6 +117,9 @@ class MyFrame(tk.Frame):
                 elif self.turnFlag == -1:
                     self.message['text'] = '✗の勝利'
             
+            #リプレイ配列に押されたボタンと挿入
+            self.boardRe.append(n)
+
             #ターン入れ替え
             if self.turnFlag == 1:
                 self.turnFlag = -1
@@ -135,8 +143,52 @@ class MyFrame(tk.Frame):
         self.b7['text'] = '　'
         self.b8['text'] = '　'
         self.b9['text'] = '　'
-        self.boardFlag = [[True]*3]*3
+        self.boardFlag = [[True]*3,[True]*3,[True]*3]
+        self.boardRe = []
         self.message['text'] = 'ゲームをリセットしました'
+
+    #ゲームのリプレイ
+    def r_key(self):
+        print('ボタンの文字初期化')
+        self.b1['text'] = '　'
+        self.b2['text'] = '　'
+        self.b3['text'] = '　'
+        self.b4['text'] = '　'
+        self.b5['text'] = '　'
+        self.b6['text'] = '　'
+        self.b7['text'] = '　'
+        self.b8['text'] = '　'
+        self.b9['text'] = '　'
+        self.message['text'] = 'リプレイ中'
+        self.after(1000)
+        for i in range(len(self.boardRe)):
+            if i%2 == 0:
+                text = "○"
+            else:
+                text = "✗"
+            
+            if i == 0:
+                self.b1['text'] = text
+            elif i == 1:
+                self.b2['text'] = text
+            elif i == 2:
+                self.b3['text'] = text
+            elif i == 3:
+                self.b4['text'] = text
+            elif i == 4:
+                self.b5['text'] = text
+            elif i == 5:
+                self.b6['text'] = text
+            elif i == 6:
+                self.b7['text'] = text
+            elif i == 7:
+                self.b8['text'] = text
+            elif i == 8:
+                self.b9['text'] = text
+            print('リプレイループ',i,' 表示文字は',text)
+        self.message['text'] = 'リプレイ終了'
+        
+            
 
 
 #メインプログラム
